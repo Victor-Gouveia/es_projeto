2668cb2264a536046a10c602480bc752ace48c42
Victor-Gouveia <victor.gouveia.133@ufrn.edu.br>
2025-06-24 16:49:30 +0000

    initial

---
+++ auth/auth.go
@@ -0,0 +1,100 @@
+package auth
+// pacote utilizado para todas as operações relacionadas
+// com autenticação de usuário, geração e checagem de token
+import  "github.com/golang-jwt/jwt/v5"
+
+// chave secreta para criptografia do token
+var secret = "chave_secreta"
+
+// struct com login, senha e cargo do usuario
+type user struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Role 	 string	`json:"role"`
+}
+
+// lista de usuarios "hard-coded"
+var users = []user{
+	{Username: "test", Password: "test", Role: ""},
+	{Username: "admin", Password: "admin", Role: "admin"},
+}
+
+
+func GenerateToken(username string, role string) string {
+	// Faz os claims pelo jwt para criar o token com eles
+	// OBS: Idealmente teria a data de expiracao do token,
+	// mas nao coloquei para simplificar implementação e
+	// testes
+	claims := jwt.MapClaims{
+		"username": username,
+		"role":    role,
+	}
+
+	// Gera o token a partir dos claims, assina em HS256
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString([]byte(secret)) // assina com o segredo
+
+	// Se houver erro, retorna uma string vazia, isso e
+	// lidado depois em LogUser
+	if err != nil {
+		return ""
+	}
+	return tokenString
+}
+
+func LogUser(username string, password string) (string, bool) {
+
+// Loop para procurar usuario e senha corretos
+	for _, a := range users {
+		if a.Username == username && a.Password == password {
+			// Obtem o cargo do usuario para gerar o token
+			var role = a.Role;
+			// Gera o token a partir do usuario dado
+			sign_token := GenerateToken(username, role)
+
+			// Se o token nao for nulo, retorna ao usuario
+			if sign_token != "" {
+				return sign_token, true
+			}
+			// Se o token for nulo, exibe erro
+			return "error generating token", false
+		}
+	}
+	// Se nao achar nenhum usuario com senha, mostra erro pro usuario
+	return "user not found or wrong password", false
+}
+
+func CheckToken(tokenString string, role string) (string, bool) {
+	// Usa jwt.Parse para obter as informacoes do token
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			// Se nao estiver codificado no metodo certo, retorna um erro
+			return nil, jwt.ErrSignatureInvalid
+		}
+		// Se tiver ok, retorna os claims
+		return []byte(secret), nil
+	})
+
+	// Em caso de erro, retorna token invalido
+	if err != nil {
+		return "invalid token", false
+	}
+
+	// Obtem os claims do token se ele for valido
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		// Primeiro, testa se a informacao de admin esta correta
+		tknRole, ok := claims["role"].(string)
+		if !ok {
+			// Se nao for valido o cargo, retorna o erro ao usuario
+			return "role sign not valid", false
+		}
+		// Se precisar de cargo e nao tiver, retorna que nao esta autorizado
+		if tknRole != role && role != "" {
+			return "token does not have required role", false
+		}
+		// Estando tudo ok, retorna que a checagem deu certo
+		return "", true
+	}
+	// Erro inesperado, esta aqui de fallback, nao deve ocorrer
+	return "there was an issue validating your token", false
+}
---
+++ main.go
@@ -0,0 +1,211 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"0.0.0.0/auth"
+)
+
+// struct com informacoes dos albums, tudo com parametros json
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
+func main() {
+	router := gin.Default()
+	//GET
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	//POST
+	router.POST("/albums", postAlbums)
+	router.POST("/auth", postAuth)
+	//PUT
+	router.PUT("/albums/:id", putAlbumID)
+	//DELETE
+	router.DELETE("/albums/:id", deleteAlbumID)
+
+	// roda o servidor localmente
+	router.Run("localhost:8080")
+
+	// OBS: Existe um meio de testar o token ao receber uma rota,
+	// mas por agora (12/06) nao irei fazer isso
+	// VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+	// admin := router.group("/")
+	// admin.Use(*Funcao para autenticar*){*rotas de admin aqui*}
+}
+
+// albums definidos "hard-coded"
+var albums = []album{
+	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getAlbums(c *gin.Context) {
+	// Faz o teste do token | Nao precisa de acesso de administrador
+	if !checkToken(c, "") {
+		return
+	}
+	// Retorna a lista de albums
+	c.IndentedJSON(http.StatusOK, albums)
+}
+
+// getAlbumByID - Acha um album pelo ID dado e exibe
+func getAlbumByID(c *gin.Context) {
+	// Faz o teste do token | Nao precisa de acesso de administrador
+	if !checkToken(c, "") {
+		return
+	}
+
+	id := c.Param("id")
+
+	// Loop para achar um album com o mesmo ID
+	for _, a := range albums {
+		if a.ID == id {
+			// Retorna o album achado ao usuario
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	// Se nao achar um album, retorna uma mensagem de erro
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+// postAlbums adds an album from JSON received in the request body.
+func postAlbums(c *gin.Context) {
+	// Faz o teste do token | Precisa de acesso de administrador
+	if !checkToken(c, "admin") {
+		return
+	}
+
+	var newAlbum album
+
+	// Usa BindJson para pegar o album novo
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Adiciona o album novo na lista
+	albums = append(albums, newAlbum)
+	c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func putAlbumID(c *gin.Context) {
+	// Faz o teste do token | Precisa de acesso de administrador
+	if !checkToken(c, "admin") {
+		return
+	}
+
+	id := c.Param("id")
+
+	var newAlbum album
+
+	if err := c.BindJSON(&newAlbum); err != nil {
+		return
+	}
+
+	// Loop para achar um album com o mesmo ID
+	for i := 0; i < len(albums); i++ {
+		if albums[i].ID == id {
+			// Achando o album, testa as entradas e se nao forem nulas
+			// substitui os valores
+			//
+			// Se nao testar antes ele coloca a string vazia
+			if newAlbum.Artist != "" {
+				albums[i].Artist = newAlbum.Artist
+			}
+			if newAlbum.Title != "" {
+				albums[i].Title = newAlbum.Title
+			}
+			if newAlbum.Price > 0 {
+				albums[i].Price = newAlbum.Price
+			}
+
+			c.IndentedJSON(http.StatusOK, albums[i])
+			return
+		}
+	}
+	// Se nao achar album, retorna um erro ao usuario
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+func deleteAlbumID(c *gin.Context) {
+	// Faz o teste do token | Precisa de acesso de administrador
+	if !checkToken(c, "admin") {
+		return
+	}
+
+	id := c.Param("id")
+
+	// Loop para achar um album com o mesmo ID
+	for i := 0; i < len(albums); i++ {
+		if albums[i].ID == id {
+			// Golang nao tem splice, entao faz um append entre tudo
+			// que ta antes do item achado com o que tiver depois
+			albums = append(albums[:i], albums[i+1:]...)
+			c.IndentedJSON(http.StatusOK, gin.H{"message": "removed album", "id": id})
+			return
+		}
+	}
+	// Se nao achar album, retorna um erro ao usuario
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
+// funções para autenticação de usuário, usando pacote
+// interno "auth" para operação, basicamente apenas
+// extrai informações necessárias para serem enviadas
+// ao pacote (24/06)
+
+// struct para obter login e senha do usuario
+type user_cred struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// Funcao de autenticacao, vulgo login
+func postAuth(c *gin.Context) {
+	var login user_cred
+	// Usa BindJson para pegar o login dado pelo usuario
+	if err := c.BindJSON(&login); err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "failed to obtain login credentials"})
+		return
+	}
+
+	msg, ok := auth.LogUser(login.Username, login.Password)
+
+	if !ok {
+		// Se houver erro, mostra ao usuario qual o erro
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": msg})
+		return
+	}
+
+	// Se estiver tudo ok, msg = token, envia ao usuario
+	c.IndentedJSON(http.StatusOK, gin.H{"token": msg})
+}
+
+// Faz a checagem to token | Alteração: converter role string em roles string[],
+// pois tem funcoes que podem ser acessadas por mais de um cargo (24/06)
+func checkToken(c *gin.Context, role string) bool {
+	// Pega o token pelo header de autorizacao
+	tokenString := c.GetHeader("Authorization")
+
+	// Se nao tiver token, avisa que nao achou o token e retorna
+	if tokenString == "" {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": "token not found"})
+		return false
+	}
+
+	msg, ok := auth.CheckToken(tokenString, role)
+
+	if !ok {
+		c.IndentedJSON(http.StatusUnauthorized, gin.H{"message": msg})
+	}
+
+	return ok
+}
